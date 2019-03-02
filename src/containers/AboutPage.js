0793fdf7ad47900ae41b28b1be4da93dfd28cda6
Corwin Marsh <corwinm@users.noreply.github.com>
2019-03-02 15:12:30 -0800

    Add apollo client for local dev with sample query

--- src/containers/AboutPage.js
+++ src/containers/AboutPage.js
@@ -1,11 +1,23 @@
 import React from 'react'
+import { Query } from "react-apollo";
+import gql from "graphql-tag";
 
-function AboutPage(props) {
-  return (
-    <div>
-      About Page!
-    </div>
-  )
-}
+const HELLO_QUERY = gql`
+{
+  hello
+}`;
 
-export default AboutPage
+const AboutPage = () => (
+  <Query query={HELLO_QUERY} >
+    {({ loading, error, data }) => {
+      if (loading) return <p>Loading...</p>;
+      if (error) return <p>Error :(</p>;
+
+      return (
+        <div>{data.hello}</div>
+      );
+    }}
+  </Query>
+);
+
+export default AboutPage;
--- src/containers/App.js
+++ src/containers/App.js
@@ -2,36 +2,44 @@ import React, { Component } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import HomePage from "./HomePage";
-import AboutPage from './AboutPage';
-import MapPage from './MapPage';
+import AboutPage from "./AboutPage";
+import MapPage from "./MapPage";
 import NoMatch from "../components/NoMatch";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+
+const client = new ApolloClient({
+  uri: "http://127.0.0.1:3000/graphql"
+});
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/map">Map</Link>
-            </li>
-          </ul>
+      <ApolloProvider client={client}>
+        <Router>
+          <div>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <Link to="/map">Map</Link>
+              </li>
+            </ul>
 
-          <hr />
-          <Switch>
-            <Route exact path="/" component={HomePage} />
-            <Route path="/about" component={AboutPage} />
-            <Route path="/map" component={MapPage} />
-            <Route component={NoMatch}/>
-          </Switch>
-        </div>
-      </Router>
+            <hr />
+            <Switch>
+              <Route exact path="/" component={HomePage} />
+              <Route path="/about" component={AboutPage} />
+              <Route path="/map" component={MapPage} />
+              <Route component={NoMatch} />
+            </Switch>
+          </div>
+        </Router>
+      </ApolloProvider>
     );
   }
 }
